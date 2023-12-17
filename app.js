5335aaf36f3abfc01eee684492e1e27e6868f787
your_name_here <your_email@email.com>
2023-12-17 19:14:25 +0530

    game

---
+++ app.js
@@ -0,0 +1,7 @@
+// Iteration 1: Make the Play button functional.
+// Description: When the Play button is clicked the game page should be displayed.
+let playBtn=document.getElementById("play-button");
+
+playBtn.onclick=()=>{
+    window.location.href="./game.html";
+}
